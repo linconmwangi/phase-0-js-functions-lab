2da5ead7dc9905fd5ba691ae59571ce48a4d489a
Lincon Thuita Mwangi <lincon.mwangi@student.moringaschool.com>
2026-03-27 12:41:11 +0300

    update the functions

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+function calculateTax(amount) {
+    let taxrate=0.1
+    return amount*taxrate
+}
+
+console. log(calculateTax(500)) 
+
+
+function convertToUpperCase(text) {
+    return text.toUpperCase()
+}
+
+console. log(convertToUpperCase("today")) 
+
+
+function findMaximum(num1,num2) {
+    if (num1 > num2) return num1
+     else return num2  
+    }
+console. log(findMaximum(25,25)) 
+
+
+function isPalindrome(word) {
+   let backwardAndForward=word.split("").reverse().join("")
+
+    if (word === backwardAndForward)
+        return true
+    else return false
+}
+
+console .log(isPalindrome ("mark")) 
+
+
+function calculateDiscountedPrice(originalPrice ,discountPercentage) {
+    originalPrice=100
+    discountPercentage=0.20
+    
+   return discountPercentage*originalPrice 
+}
+console .log(calculateDiscountedPrice())
 
 
 
